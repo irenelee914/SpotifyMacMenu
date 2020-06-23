2fd45ea3903aa926b19a94f7ad596693971472b0
Irene Lee <irenelee914@gmail.com>
2020-06-23 19:42:40 -0400

    Initial Commit

---
+++ SpotifyMacMenu/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SpotifyMacMenu
+//
+//  Created by Hojoon Lee on 2020-06-23.
+//  Copyright © 2020 Irene Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
